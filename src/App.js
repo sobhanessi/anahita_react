7e15ea537f032930d644c373919dfd2ac1349845
sobhanessi <sobhanessi@gmail.com>
2022-06-24 20:59:50 +0300

    the structure of project has been fundamented

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Backend from "i18next-http-backend";
+import Homepage from "./pages/Homepage";
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import Menu from "./pages/Menu";
+
+i18n.use(Backend).use(initReactI18next).init({
+  lng: "en",
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/" element={<Homepage />} />
+      </Routes>
+    </Router>
   );
 }
 
