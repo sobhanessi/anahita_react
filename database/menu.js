aeb9e8576c28c9ca8a2f5a6a2dc108f8c231c3fd
sobhanessi <sobhanessi@gmail.com>
2022-08-08 20:06:33 +0200

    working on the migration from react to next js

---
+++ database/menu.js
@@ -0,0 +1,429 @@
+// import kebabBargWithoutRice from "/menu/kebabbargwithoutrice.jpg";
+const menu = [
+  {
+    APPETIZERS: [
+      {
+        name: "OLIVIEH",
+        pic: "/menu/olivieh.jpg",
+        description: "",
+        price: "4.00",
+      },
+      {
+        name: "MAST_MOUSIR",
+        pic: "/menu/mast_mousir.jpg",
+        description: "",
+        price: "4.00",
+      },
+      {
+        name: "BOURANI",
+        pic: "/menu/borani.jpg",
+        description: "",
+        price: "4.00",
+      },
+      {
+        name: "NARGESI",
+        pic: "/menu/nargesi.jpg",
+        description: "",
+        price: "4.00",
+      },
+      {
+        name: "NAZKHATOON",
+        pic: "/menu/nazkhatoon.jpg",
+        description: "",
+        price: "4.00",
+      },
+      {
+        name: "MASTO_KHIAR",
+        pic: "/menu/mastokhiar.jpg",
+        description: "",
+        price: "4.00",
+      },
+      {
+        name: "KASHKE_BADEMJAN",
+        pic: "/menu/kashkebademjoon",
+        description: "",
+        price: "7.00",
+      },
+      {
+        name: "MIRZA_GHASEMI",
+        pic: "/menu/mirzaghasemi.jpg",
+        description: "",
+        price: "7.00",
+      },
+      {
+        name: "KOUFTEH_BERENJI",
+        pic: "/menu/kooftehberenji.jpg",
+        description: "",
+        price: "6.50",
+      },
+      {
+        name: "KOTLET",
+        pic: "/menu/kotlet.jpg",
+        description: "",
+        price: "6.50",
+      },
+      {
+        name: "LAKHMAJOON",
+        pic: "/menu/lahmajoon.jpg",
+        description: "",
+        price: "6.50",
+      },
+      {
+        name: "FRIED_POTATOES",
+        pic: "/menu/friedpotatoes.jpg",
+        description: "",
+        price: "3.00",
+      },
+      {
+        name: "FRIED_POTATOES_WITH_CHEESE",
+        pic: "/menu/friedpotatoeswithcheese.jpg",
+        description: "",
+        price: "3.50",
+      },
+      {
+        name: "PERSIAN_BREAD",
+        pic: "/menu/persianbread.jpg",
+        description: "",
+        price: "1.50",
+      },
+      {
+        name: "ASH_RESHTEH",
+        pic: "/menu/ashreshteh.jpg",
+        description: "",
+        price: "5.00",
+      },
+      {
+        name: "JO_SOUP",
+        pic: "/menu/josoup.jpg",
+        description: "",
+        price: "5.00",
+      },
+      {
+        name: "SAGANAKI_CHEESE_WITH_RED_SAUCE",
+        pic: "/menu/saganaki.jpg",
+        description: "",
+        price: "4.00",
+      },
+      {
+        name: "CHEESE_CROQUETTES_WITH_RED_SAUCE",
+        pic: "/menu/cheesecroquettes.jpg",
+        description: "",
+        price: "4.00",
+      },
+      {
+        name: "SOSIS_BANDARI",
+        pic: "/menu/sosisbandari.jpg",
+        description: "",
+        price: "4.00",
+      },
+      {
+        name: "HUMMUS",
+        pic: "/menu/hummus.jpg",
+        description: "",
+        price: "4.00",
+      },
+    ],
+  },
+  {
+    MAIN_COURSE: [
+      {
+        name: "JOUJE_KEBAB",
+        pic: "/menu/joujehkebab.jpg",
+        description: "",
+        price: "10.00",
+      },
+      {
+        name: "KEBAB_BARG",
+        pic: "/menu/kebabbarg.jpg",
+        description: "",
+        price: "12.00",
+      },
+      {
+        name: "KEBAB_KOUBIDEH",
+        pic: "/menu/kebabkoubideh.jpg",
+        description: "",
+        price: "10.00",
+      },
+      {
+        name: "CHELO_KEBAB",
+        pic: "/menu/chelokebab.jpg",
+        description: "",
+        price: "12.00",
+      },
+      {
+        name: "KEBAB_CHENJEH",
+        pic: "/menu/kebabchenjeh.jpg",
+        description: "",
+        price: "12.00",
+      },
+      {
+        name: "SOLTANI_WITH_RICE",
+        pic: "/menu/kebabsoltaniwithrice.jpg",
+        description: "",
+        price: "13.00",
+      },
+      {
+        name: "SOLTANI_WITHOUT_RICE",
+        pic: "/menu/kebabsoltani.jpg",
+        description: "",
+        price: "15.00",
+      },
+      {
+        name: "KEBAB_MIX_FOR_2",
+        pic: "/menu/kebabmix.jpg",
+        description: "",
+        price: "22.00",
+      },
+      {
+        name: "KEBAB_MIX_FOR_4",
+        pic: "/menu/kebabmixfor4.jpg",
+        description: "",
+        price: "34.00",
+      },
+      {
+        name: "SUPER_DISHES",
+        pic: "/menu/superdish.jpg",
+        description: "",
+        price: "55.00",
+      },
+      {
+        name: "ZERESHK_POLO",
+        pic: "/menu/zereshkpolo.jpg",
+        description: "",
+        price: "12.00",
+      },
+      {
+        name: "SHIRIN_POLO",
+        pic: "/menu/shirinpolo.jpg",
+        description: "",
+        price: "12.00",
+      },
+      {
+        name: "ZERESHK_POLO_OR_SHIRIN_POLO",
+        pic: "/menu/zereshkpolowithoutrice.jpg",
+        description: "",
+        price: "6.50",
+      },
+      {
+        name: "BAGHALI_POLO",
+        pic: "/menu/baghalipolo.jpg",
+        description: "",
+        price: "12.00",
+      },
+      {
+        name: "KHORESHTE_GHEIME",
+        pic: "/menu/khoreshtehgheimeh.jpg",
+        description: "",
+        price: "12.00",
+      },
+      {
+        name: "KHORESHTE_ESFENAJ",
+        pic: "/menu/khoreshtehesfenaj.jpg",
+        description: "",
+        price: "12.00",
+      },
+      {
+        name: "KHORESHTE_GHORME_SABZI",
+        pic: "/menu/khoreshtehghormehsabzi.jpg",
+        description: "",
+        price: "12.00",
+      },
+      {
+        name: "KHORESHTE_FESENJAN",
+        pic: "/menu/khoreshtehfesenjan.jpg",
+        description: "",
+        price: "13.00",
+      },
+      {
+        name: "BIFTEKI_SPECIAL_WITH_CHEESE_FILLING",
+        pic: "/menu/hamburgerspecial.jpg",
+        description: "",
+        price: "10.00",
+      },
+      {
+        name: "PERSIAN_RICE",
+        pic: "/menu/persianrice.jpg",
+        description: "",
+        price: "4.00",
+      },
+    ],
+  },
+  {
+    SALADS: [
+      {
+        name: "ANAHITA_SALAD",
+        pic: "",
+        description: "/menu/anahitasalad.jpg",
+        price: "7.00",
+      },
+      {
+        name: "PERSIAN_SALAD",
+        pic: "/menu/persiansalad.jpg",
+        description: "",
+        price: "7.00",
+      },
+      {
+        name: "KARAFS_SALAD",
+        pic: "/menu/karafssalad.jpg",
+        description: "",
+        price: "6.00",
+      },
+      {
+        name: "SHIRAZI_SALAD",
+        pic: "/menu/shirazisalad.jpg",
+        description: "",
+        price: "6.00",
+      },
+      {
+        name: "GREEN_SALAD",
+        pic: "/menu/greensalad.jpg",
+        description: "",
+        price: "8.00",
+      },
+      {
+        name: "SALAD_ROQUEFORT",
+        pic: "/menu/saladwithroquefort.jpg",
+        description: "",
+        price: "8.00",
+      },
+    ],
+  },
+  {
+    BEVERAGES: [
+      {
+        name: "COCA_COLA",
+        pic: "",
+        description: "",
+        price: "1.50",
+      },
+      {
+        name: "COCA_COLA_LIGHT",
+        pic: "",
+        description: "",
+        price: "1.50",
+      },
+      {
+        name: "COCA_COLA_ZERO",
+        pic: "",
+        description: "",
+        price: "1.50",
+      },
+      {
+        name: "SPRITE",
+        pic: "",
+        description: "",
+        price: "1.50",
+      },
+      {
+        name: "FANTA",
+        pic: "",
+        description: "",
+        price: "1.50",
+      },
+      {
+        name: "SODA",
+        pic: "",
+        description: "",
+        price: "1.50",
+      },
+      {
+        name: "SPARKLING_WATER",
+        pic: "",
+        description: "",
+        price: "1.50",
+      },
+      {
+        name: "WATER",
+        pic: "",
+        description: "",
+        price: "2.00",
+      },
+      {
+        name: "DOUGH",
+        pic: "",
+        description: "",
+        price: "3.00",
+      },
+    ],
+  },
+  {
+    WINE: [
+      {
+        name: "WHITE_WINE",
+        pic: "",
+        description: "",
+        price: "5.00",
+      },
+      {
+        name: "RED_WINE",
+        pic: "",
+        description: "",
+        price: "5.00",
+      },
+    ],
+  },
+  {
+    COFFEE_TEA: [
+      {
+        name: "ESPRESSO",
+        pic: "",
+        description: "",
+        price: "2.50",
+      },
+      {
+        name: "FILTER_COFFEE",
+        pic: "",
+        description: "",
+        price: "2.50",
+      },
+      {
+        name: "GREEK_COFFEE",
+        pic: "",
+        description: "",
+        price: "2.00",
+      },
+      {
+        name: "PERSIAN_TEA",
+        pic: "",
+        description: "",
+        price: "1.50",
+      },
+    ],
+  },
+  {
+    DESSERTS: [
+      {
+        name: "PERSIAN_ICE_CREAM",
+        pic: "/menu/persianicecream.jpg",
+        description: "Saffron - Peanuts - Rose Water",
+        price: "3.50",
+      },
+      {
+        name: "EKMEK_WITH_ICE_CREAM",
+        pic: "",
+        description: "",
+        price: "5.00",
+      },
+      {
+        name: "WALNUT_PIE_WITH_ICE_CREAM",
+        pic: "",
+        description: "",
+        price: "5.00",
+      },
+      {
+        name: "RAVANI_WITH_ICE_CREAM",
+        pic: "",
+        description: "",
+        price: "5.00",
+      },
+      {
+        name: "FALOUDE",
+        pic: "/menu/faloudeh.jpg",
+        description: "",
+        price: "4.00",
+      },
+    ],
+  },
+];
+
+export default menu;
---
+++ pages/menu.tsx
@@ -0,0 +1,125 @@
+import React from "react";
+import {
+  Card,
+  CardContent,
+  CardMedia,
+  Container,
+  Divider,
+  Grid,
+  Typography,
+} from "@mui/material";
+import { I18nContext, useTranslation } from "next-i18next";
+
+import menu from "../database/menu";
+
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+
+export async function getStaticProps({ locale }) {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ["common"])),
+      // Will be passed to the page component as props
+    },
+  };
+}
+
+function Menu(): JSX.Element {
+  const { t, i18n } = useTranslation();
+
+  // console.log(
+  //   menu.map((mm) =>
+  //     Object.keys(mm).map((m) => mm[m].map((men) => console.log(men.pic)))
+  //   )
+  // );
+
+  return (
+    <Container>
+      <Grid container justifyContent={"center"}>
+        {menu.map((mm) => (
+          <>
+            {Object.keys(mm).map((m) => (
+              <>
+                <Grid item sm={8} md={8} lg={8} xs={10} xl={8}>
+                  <Typography
+                    gutterBottom
+                    sx={{
+                      mt: 4,
+                      mb: 4,
+                      fontWeight: "bold",
+                      fontSize: "1.75em",
+                    }}
+                  >
+                    {m === "MAIN_COURSE"
+                      ? "MAIN COURSE"
+                      : m === "COFFEE_TEA"
+                      ? "COFFEE TEA"
+                      : m}
+                  </Typography>
+                  {mm[m].map((men) => (
+                    <>
+                      <Card sx={{ mt: 2, mb: 2 }} key={men.name}>
+                        <CardMedia image={men.pic} component="img" />
+                        <CardContent>
+                          <Grid container>
+                            <Grid item sm={11}>
+                              <Typography
+                                component="h6"
+                                variant="h6"
+                                sx={{
+                                  fontWeight: 700,
+                                  fontSize: "1.5em",
+                                  mr: 1,
+                                }}
+                              >
+                                {t(`MENU.${m}.${men.name}.NAME`)}
+                              </Typography>
+                              <Typography
+                                component="div"
+                                variant="body2"
+                                sx={{
+                                  fontSize: "1.2em",
+                                  fontWeight: 500,
+                                  mr: 2,
+                                }}
+                              >
+                                {i18n.exists(
+                                  `MENU.${m}.${men.name}.DESCRIPTION`
+                                ) && t(`MENU.${m}.${men.name}.DESCRIPTION`)}
+                              </Typography>
+                            </Grid>
+                            <Grid
+                              item
+                              sm={1}
+                              sx={{
+                                display: "flex",
+                                alignItems: "center",
+                              }}
+                            >
+                              <Typography
+                                sx={{
+                                  fontWeight: "bold",
+                                  display: "flex",
+                                  flexDirection: "row",
+                                }}
+                              >
+                                <span> € </span>
+                                <span>{t(`MENU.${m}.${men.name}.PRICE`)}</span>
+                              </Typography>
+                            </Grid>
+                          </Grid>
+                        </CardContent>
+                      </Card>
+                      <Divider />
+                    </>
+                  ))}
+                </Grid>
+              </>
+            ))}
+          </>
+        ))}
+      </Grid>
+    </Container>
+  );
+}
+
+export default Menu;
